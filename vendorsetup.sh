6e9082f0b9c49a2feca28523ea211247f4cede44
ris <rissu.ntk@gmail.com>
2026-06-28 11:38:34 +0700

    a05m: Import jdm-vibration patches
    Co-authored-by: @ravindu644
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_a05m-user
-add_lunch_combo omni_a05m-userdebug
-add_lunch_combo omni_a05m-eng
+DEVICE_PATH=device/samsung/a05m
+
+echo "Starting vendorsetup.sh"
+
+jdm_patches_exist=$(grep -q "USE_SAMSUNG_JDM_HAPTICS" bootable/recovery/minuitwrp/events.cpp; echo $?)
+
+if [ $jdm_patches_exist != 0 ]; then
+	patch -p1 --no-backup-if-mismatch < $(DEVICE_PATH)/patches/01*.patch
+	patch -p1 --no-backup-if-mismatch < $(DEVICE_PATH)/patches/02*.patch
+	echo "Patches applied."
+else
+	echo "Patches skipped. Already applied."
+fi
